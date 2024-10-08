8e6654992ad68de4f2a9a116756b094baea1035b
delaneybroderick <delaneyb@unc.edu>
2024-10-08 14:16:46 -0400

    Add files via upload

---
+++ assignment_03/assignment03.py
@@ -0,0 +1,10 @@
+
+songs = ['Girl', 'Yesterday','I Know its Over', 'Unloveable']
+
+for song in songs:
+    if song == 'Girl':
+        print('The Beatles')
+    if song == 'Yesterday':
+        print('The Beatles')
+    else:
+        print('The Smiths')
---
+++ assignment_03/assingment03.5.py
@@ -0,0 +1,16 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+    
+
+items = ['item1', 'item2', 'item3', 'item4']
+
+for item in items:
+    if item == "item3":
+        print(item.title()) # you may use any string method you wish.
+    else:
+        print(item.upper()) # you man use any string method you wish.
