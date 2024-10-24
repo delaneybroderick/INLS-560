057acb76525f19861c846f1d19ddc6fb4d24a0ab
delaneybroderick <delaneyb@unc.edu>
2024-10-24 13:46:19 -0400

    Update amusement_park.py

--- amusement_park.py
+++ amusement_park.py
@@ -4,9 +4,11 @@ if age < 4:
     print("Admission is $0")
 elif age < 18:
     print("Admission is $25")
-elif age > 60:
-    print("Admission is $35")
 elif age > 100:
     print("Admission is $0 and you get a free beer")
+elif age > 60:
+    print("Admission is $35")
 else:
     print("Admission cost $40")
+
+
